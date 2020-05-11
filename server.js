868a95e26164779bf3da9dbcb66263884bc485f8
Chris Cosentino <chriscosentino@hotmail.com>
2020-05-11 15:21:00 -0400

    changes made

--- server.js
+++ server.js
@@ -17,24 +17,6 @@ app.use(cors({credentials: true, origin: true}));
 //BodyParser Middleware
 app.use(bodyParser.json());
 
-//DB Config
-const db = require('./config/keys').mongoURI;
-
-
-// app.use(express.static(path.join(__dirname, 'client/build')));
-
-
-// if(process.env.NODE_ENV === 'production') {  
-//     app.use(express.static(path.join(__dirname, 'client/build')));  
-
-//     app.get('*', (req, res) => {   
-//        res.sendfile(path.join(__dirname = 'client/build/index.html')); 
-//      })
-// }else{
-//     app.get('*', (req, res) => { 
-//         res.sendFile(path.join(__dirname+'/client/public/index.html'));
-//    })
-// }
 
 mongoose
     .connect(process.env.MONGODB_URI, {
@@ -47,14 +29,6 @@ mongoose
 
 app.use('/api/links', links);
 
-
-// if(process.env.MODE_ENV === 'production'){
-//     app.use('/', express.static( 'client/build' ));
-
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-//     })
-// }
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
